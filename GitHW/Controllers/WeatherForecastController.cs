9c03ca199b85df255a8450720fd05258a11b4ec0
PlaceA101 <placean@mail.uc.edu>
2024-11-03 11:36:02 -0500

    New changed files

--- GitHW/Controllers/WeatherForecastController.cs
+++ GitHW/Controllers/WeatherForecastController.cs
@@ -1,33 +1,15 @@
 using Microsoft.AspNetCore.Mvc;
 
-namespace GitHW.Controllers
+namespace AidanNamespace.Controllers
 {
     [ApiController]
     [Route("[controller]")]
-    public class WeatherForecastController : ControllerBase
+    public class NameController : ControllerBase
     {
-        private static readonly string[] Summaries = new[]
+        [HttpGet]
+        public IActionResult GetName()
         {
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-        };
-
-        private readonly ILogger<WeatherForecastController> _logger;
-
-        public WeatherForecastController(ILogger<WeatherForecastController> logger)
-        {
-            _logger = logger;
-        }
-
-        [HttpGet(Name = "GetWeatherForecast")]
-        public IEnumerable<WeatherForecast> Get()
-        {
-            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
-            {
-                Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-                TemperatureC = Random.Shared.Next(-20, 55),
-                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
-            })
-            .ToArray();
+            return Ok(new { name = "Aidan Place" });
         }
     }
 }
